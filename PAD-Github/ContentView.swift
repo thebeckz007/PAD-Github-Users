e9ac09799a7091010302bf62370f762db7d8e9f0
Duy Phan <thebeckz007@gmail.com>
2024-09-09 10:31:26 +0700

    Initial Commit

---
+++ PAD-Github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PAD-Github
+//
+//  Created by Phan Anh Duy on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PAD-Github/PAD_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAD_GithubApp.swift
+//  PAD-Github
+//
+//  Created by Phan Anh Duy on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PAD_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
